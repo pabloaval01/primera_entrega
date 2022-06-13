f96418f516e2b0a959860ec8522a2d71343a238b
pabloaval01 <105010632+pabloaval01@users.noreply.github.com>
2022-06-13 17:23:29 -0300

    Delete ProyectoCoder directory

--- ProyectoCoder/AppCoder/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from AppCoder.views import curso, profesores, inicio
-
-urlpatterns = [
-    path('curso/', curso),
-    path('profesores/', profesores),
-    path('', inicio),
-]
--- ProyectoCoder/ProyectoCoder/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from AppCoder.views import curso, profesores
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('appCoder/', include('AppCoder.urls')),
-]
